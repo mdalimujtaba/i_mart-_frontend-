2610f82594849d068783c11bdbe07aaaebdd31c8
mdalimujtaba <mdalimujtaba29850@gmail.com>
2023-02-09 02:08:47 +0530

    added component and functionality to cart page

--- src/Components/cartCard.jsx
+++ src/Components/cartCard.jsx
@@ -1,38 +1,80 @@
-import { Box, Button, Image, Text } from "@chakra-ui/react"
+import { Box, Button, Image, Text } from "@chakra-ui/react";
 import axios from "axios";
 import { useEffect } from "react";
 
-
-export const CartCard=(product)=>{
-   
-    let token=JSON.parse(localStorage.getItem('token'))
-    // const handleDelete=()=>{
-    //     axios.delete(`http://localhost:8080/cart/delete/${product.id}`,{headers:{'Authorization':token}})
-    // }
-    // useEffect(()=>{
-    //     handleDelete()
-    // },[])
-    let bag = "";
-    let n=product.product.rating
+export const CartCard = ({
+  productID,
+  handleDelete,
+  _id,
+  quantity,
+  handleDecrease,
+  handleIncrease,
+}) => {
+  let token = JSON.parse(localStorage.getItem("token"));
+  let bag = "";
+  let n = productID.rating;
   for (let i = 0; i < n; i++) {
     bag = bag + "\u2B50";
   }
-    return(
-        <>
-        <Box border={'4px solid white'} h={['250px']}  w={['250px']} display={['flex']}  width={['100%']}>
-            <Box border={'1px solid grey'} w={['250px']}>
-                <Image src={product.product.image} h={['100%']} w={['100%']}></Image>
-            </Box>
-            <Box border={'1px solid grey'} textAlign={['right']}  w={['500px']}>
-                <Text fontSize={['22px']} fontWeight={['500']}>{product.product.title}</Text>
-                <Text mt={["15px"]}>{bag}</Text>
-                <Text fontSize={["20px"]} fontWeight={["400"]} mt={["15px"]}>
-                    Price : &#x20B9;{product.product.price}
-                </Text>
-                <Button  colorScheme={'blue'}mt={["15px"]}>Delete</Button>
-            </Box>
+  return (
+    <>
+      <Box
+        h={["auto", "auto", "auto", "250px"]}
+        display={["flex", "flex", "flex", "flex"]}
+        justifyContent={["center"]}
+        width={["100%"]}
+        mt={["10px"]}
+      >
+        <Box border={"1px solid grey"} w={["100px", "250px", "250px", "250px"]}>
+          <Image src={productID.image} h={["100%"]} w={["100%"]}></Image>
         </Box>
-        
-        </>
-    )
-}
+        <Box
+          border={"1px solid grey"}
+          textAlign={["right"]}
+          pr={["10px"]}
+          w={["100%", "100%", "500px", "500px"]}
+        >
+          <Text
+            fontSize={["18px", "18px", "22px", "22px"]}
+            fontWeight={["500"]}
+          >
+            {productID.title}
+          </Text>
+          <Text mt={["15px"]}>{bag}</Text>
+          <Text
+            fontSize={["18px", "18px", "20px", "20px"]}
+            fontWeight={["400"]}
+            mt={["15px"]}
+          >
+            Price : &#x20B9;{productID.price}
+          </Text>
+          <Box mt={["15px"]}>
+            <Button
+              onClick={() => handleDecrease(_id, "dec")}
+              disabled={quantity == 1}
+              fontSize={["20px"]}
+              colorScheme={"cyan"}
+            >
+              -
+            </Button>
+            <Button fontSize={["20px"]}>{quantity}</Button>
+            <Button
+              onClick={() => handleIncrease(_id, "inc")}
+              fontSize={["20px"]}
+              colorScheme={"cyan"}
+            >
+              +
+            </Button>
+          </Box>
+          <Button
+            onClick={() => handleDelete(_id)}
+            colorScheme={"blue"}
+            mt={["15px"]}
+          >
+            Delete
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+};
--- src/Pages/cartPage.jsx
+++ src/Pages/cartPage.jsx
@@ -1,41 +1,173 @@
-import { Box, SimpleGrid } from "@chakra-ui/react"
-import axios from "axios"
-import { useEffect, useState } from "react"
-import { CartCard } from "../Components/cartCard"
-import Navbar from "../Components/Navbar"
+import { Box, SimpleGrid, Text } from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { CartCard } from "../Components/cartCard";
+import Navbar from "../Components/Navbar";
 
-export const CartPage=()=>{
-    const [data,setData]=useState([])
-   
-    const CartItem=()=>{
-        const token=JSON.parse(localStorage.getItem("token"))
-        axios.get(`http://localhost:8080/cart/`,{headers:{"Authorization":token}})
-        .then((res)=>{
-            console.log(res)
-            setData(res.data.data)
-            
-        })
-    }
-    useEffect(()=>{
-        CartItem()
-    },[])
-    
-    return(
-        <>
-        <Navbar/>
-        <Box border={'1px solid black'} w={'90%'} ml={['5%']} display={['flex']}>
-            {/* <Box border={'1px solid red'} w={['60%']} h={['250px']} display={['flex']}>
-                <Box border={'1px solid red'} w={['40%']}></Box>
-                <Box border={'1px solid red'} w={['60%']}></Box>
-            </Box> */}
-            <Box border={'5px solid black'} height={['auto']} >
-                {data && data.map((item)=>{
-                  return  <CartCard product={item.productID} id={item._id}/>
-                })}
-            </Box>
-            <Box border={'1px solid blue'} w={['40%']}></Box>
+export const CartPage = () => {
+  const [data, setData] = useState([]);
 
+  const token = JSON.parse(localStorage.getItem("token"));
+  const CartItem = () => {
+    axios
+      .get(`http://localhost:8080/cart/`, { headers: { Authorization: token } })
+      .then((res) => {
+        // console.log(res)
+        setData(res.data.data);
+      });
+  };
+  useEffect(() => {
+    CartItem();
+  }, []);
+  const handleDelete = (id) => {
+    axios
+      .delete(`http://localhost:8080/cart/delete/${id}`, {
+        headers: { Authorization: token },
+      })
+      .then((res) => {
+        // console.log(res)
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    CartItem();
+  };
+
+  const handleDecrease = (id, type) => {
+    console.log(type, id);
+    axios
+      .patch(
+        "http://localhost:8080/cart/update",
+        { type: type, productID: id },
+        { headers: { Authorization: token } }
+      )
+      .then((res) => {
+        console.log(res);
+        CartItem();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const handleIncrease = (id, type) => {
+    console.log(type, id);
+    axios
+      .patch(
+        "http://localhost:8080/cart/update",
+        { type: type, productID: id },
+        { headers: { Authorization: token } }
+      )
+      .then((res) => {
+        console.log(res);
+        CartItem();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  let total = 0;
+  let totalItem = 0;
+  let delivery = 70;
+  data.map((item) => {
+    total = total + item.productID.price * item.quantity;
+    totalItem = totalItem + item.quantity;
+  });
+  let sgst = Math.floor((9 * total) / 100);
+  let cgst = Math.floor((9 * total) / 100);
+  let finalTotal = total + delivery + sgst + cgst;
+
+  return (
+    <>
+      <Navbar />
+      <Box>
+        <Box w={"90%"} ml={["5%"]}>
+          <Text fontWeight={[500]} fontSize={["25px", "25px", "30px", "30px"]}>
+            Shopping Cart
+          </Text>
         </Box>
-        </>
-    )
-}
+        <Box
+          w={["90%"]}
+          ml={["5%"]}
+          display={["", " ", " ", "flex"]}
+          justifyContent={["space-between"]}
+        >
+          <Box w={["", "100%", "100%", "60%"]} height={["auto"]}>
+            {data &&
+              data.map((item) => {
+                return (
+                  <CartCard
+                    key={item._id}
+                    {...item}
+                    handleDelete={handleDelete}
+                    handleDecrease={handleDecrease}
+                    handleIncrease={handleIncrease}
+                  />
+                );
+              })}
+          </Box>
+          <Box h={["auto"]} w={["", "100%", "100%", "40%"]} mt={["10px"]}>
+            <Box border={"1px solid grey"}>
+              <Text fontSize={["25px"]} mt={["15px"]}>
+                Shopping Summary
+              </Text>
+              <Box
+                p={["10px"]}
+                display={["flex"]}
+                justifyContent={["space-between"]}
+              >
+                <Text fontSize={["20px"]}>No. of items:</Text>
+                <Text fontSize={["20px"]}>{totalItem}</Text>
+              </Box>
+              <Box
+                p={["10px"]}
+                display={["flex"]}
+                justifyContent={["space-between"]}
+              >
+                <Text fontSize={["20px"]}>Total Cost:</Text>
+                <Text fontSize={["20px"]}>&#x20B9;{total}</Text>
+              </Box>
+              <Box
+                p={["10px"]}
+                display={["flex"]}
+                justifyContent={["space-between"]}
+              >
+                <Text fontSize={["20px"]}>SGST:</Text>
+                <Text fontSize={["20px"]}>&#x20B9;{sgst}</Text>
+              </Box>
+              <Box
+                p={["10px"]}
+                display={["flex"]}
+                justifyContent={["space-between"]}
+              >
+                <Text fontSize={["20px"]}>CGST:</Text>
+                <Text fontSize={["20px"]}>&#x20B9;{cgst}</Text>
+              </Box>
+              <Box
+                p={["10px"]}
+                display={["flex"]}
+                justifyContent={["space-between"]}
+              >
+                <Text fontSize={["20px"]}>Delivery:</Text>
+                <Text fontSize={["20px"]}>&#x20B9;{delivery}</Text>
+              </Box>
+              <Box
+                borderTop={"1px solid black"}
+                borderBottom={"1px solid black"}
+                p={["10px"]}
+                display={["flex"]}
+                justifyContent={["space-between"]}
+              >
+                <Text fontSize={["20px"]} fontWeight={"600"}>
+                  SubTotal:
+                </Text>
+                <Text fontSize={["20px"]} fontWeight={"600"}>
+                  &#x20B9;{finalTotal}
+                </Text>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+};
